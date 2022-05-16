db0f2092ad6836fdc498b3fdd7754fe80af8c66e
medhatnar <narnarshahin@gmail.com>
2022-05-16 13:50:07 -0400

    --fixup^

--- database/db.js
+++ database/db.js
@@ -2,17 +2,19 @@ const Sequelize = require("sequelize");
 const models = require("./models");
 
 const sequelize = new Sequelize({
-  dialect: "sqlite",
-  storage: "./database.sqlite",
+  dialect: 'sqlite',
+  storage: './database.sqlite'
 });
 
 models(sequelize);
 
 function applyAssociations(db) {
-  const { user, cat } = db.models;
+	const { user, cat, session } = db.models;
 
-  user.hasMany(cat);
-  cat.belongsTo(user);
+	user.hasMany(cat);
+	cat.belongsTo(user);
+  user.hasOne(session);
+  session.belongsTo(user);
 
   return db;
 }
